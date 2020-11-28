a74a17277eea2aae044bd9be205b850a834aa3a3
jlbuild <juliabuildbot@gmail.com>
2020-11-28 16:32:15 +0000

    SQLite_jll build 3.33.0+0

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -2,52 +2,15 @@
 export libsqlite
 
 using Zlib_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `libsqlite`
-const libsqlite_splitpath = ["lib", "libsqlite3.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libsqlite_path = ""
-
-# libsqlite-specific global declaration
-# This will be filled out by __init__()
-libsqlite_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libsqlite = "libsqlite3.so.0"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("SQLite")
+JLLWrappers.@declare_library_product(libsqlite, "libsqlite3.so.0")
 function __init__()
-    global artifact_dir = abspath(artifact"SQLite")
+    JLLWrappers.@generate_init_header(Zlib_jll)
+    JLLWrappers.@init_library_product(
+        libsqlite,
+        "lib/libsqlite3.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Zlib_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Zlib_jll.LIBPATH_list,))
-
-    global libsqlite_path = normpath(joinpath(artifact_dir, libsqlite_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libsqlite_handle = dlopen(libsqlite_path)
-    push!(LIBPATH_list, dirname(libsqlite_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
